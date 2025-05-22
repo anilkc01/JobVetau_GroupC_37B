79a55fd77332a4266bb9e8bb187eda91d7d84b88
GADWEE <adwitiya00.pandey@gmail.com>
2025-05-22 13:30:15 +0545

    Hello

---
+++ jobvetau.java
@@ -0,0 +1,18 @@
+import java.awt.*;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import java.io.File;
+import javax.swing.*;
+
+public class jobvetau {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Job Vetau");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(1920, 1080);
+        frame.setLocationRelativeTo(null);
+        frame.setLayout(null);
+        frame.getContentPane().setBackground(Color.WHITE);
+    }
+
+    
+}
